b8711f4088da3e548939ada6e0b419575e4f9349
Neeiser <stortini.study@gmail.com>
2022-08-25 15:26:18 +0200

    final light commit

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { DirectionalLight } from 'three'
 
 /**
  * Base
@@ -18,14 +19,34 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
+
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01)
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 1)
+pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
+
+gui.add(pointLight, 'distance').min(0.3).max(3).step(0.001)
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
+rectAreaLight.position.set(-1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+gui.add(rectAreaLight, 'intensity').min(0).max(5).step(0.01)
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
 
 /**
  * Objects
